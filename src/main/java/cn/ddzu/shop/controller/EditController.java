5c67366c2c60add8786b102ec4164bd68473c41f
edwin152 <15216666215@126.com>
2019-05-26 19:33:28 +0800

    新增咨询接口

--- src/main/java/cn/ddzu/shop/controller/EditController.java
+++ src/main/java/cn/ddzu/shop/controller/EditController.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.enterprise.inject.New;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
