d4fc81f8da66eae0934f1a7ce38db16de272a0fe
edwin152 <15216666215@126.com>
2019-05-28 14:24:00 +0800

    新增咨询接口

--- src/main/java/cn/ddzu/shop/controller/EditController.java
+++ src/main/java/cn/ddzu/shop/controller/EditController.java
@@ -869,7 +869,7 @@ public class EditController extends BaseController {
     /**
      * 同步所有数据
      */
-    @RequestMapping("/sync")
+//    @RequestMapping("/sync")
     public void sync(HttpServletRequest request, HttpServletResponse response) throws IOException {
         request.setCharacterEncoding("utf-8");
         response.setCharacterEncoding("utf-8");
